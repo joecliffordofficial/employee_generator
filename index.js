e31528fc65e71c0a504a829af2024798b525ebab
Joseph Clifford <joecliffordofficial@gmail.com>
2021-11-19 12:57:45 -0800

    more code added

--- index.js
+++ index.js
@@ -4,9 +4,48 @@ const Manager = require('./type/manager')
 const Engineer = require('./type/engineer')
 const Intern = require('./type/intern')
 const Employee = require('./type/employee')
+const { response } = require('express')
 
 
 let team = []
 let cardString = ''
 let memberRole = ''
 
+const managerQuestions = [
+    {
+        type: 'input',
+        message: 'What is the name of the office manager?',
+        name: 'name',
+        validate: (response) => {
+            if (!response) {
+                return console.log("This field is required.")
+            } else {
+                return true
+            }
+        }
+    }, 
+    {
+        type: 'input',
+        message: 'What is the employee ID for the office manager?',
+        name: 'id',
+        validate: (response) => {
+            if (!response) {
+                console.log('This field is required.')
+            } else {
+                return true
+            }
+        }
+    }, 
+    {
+        type: 'input',
+        message: 'What is the email address for the office manager?',
+        name: 'email',
+        validate: (response) => {
+            if (!response) {
+                console.log('This field is required.')
+            } else {
+                return true
+            }
+        }
+    }, 
+]
