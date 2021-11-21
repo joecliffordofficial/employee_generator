78882f2e5574359b26ee37a39e620036a8542723
Joseph Clifford <joecliffordofficial@gmail.com>
2021-11-20 19:46:49 -0800

    html piece

--- index.js
+++ index.js
@@ -251,7 +251,35 @@ const createCards = (team) => {
     return cardString
 }
 
+const writeToFile = (fileName, data) => {
+    fs.writeFile(`${fileName}`, data, (err) => {
+        err ? console.error('Error! : ' + err) : console.log('Success! You made your HTML!')
+    })
+}
 
+const generateHtml = (cards) => {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title>Team Profile Generator</title>
+    </head>
+    <body class="text-center">
+        <nav class="navbar navbar-light bg-danger"><h1 class="mx-auto">My Team</h1></nav>
+        <div class="container">
+        <div class="d-flex flex-row flex-wrap">
+        ${cards}
+        </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+    </html>`
+}
 
 
 function init() {
